5935a9ce708c9cf4772007cc759a9c9d0835d849
DasNataliia <dasnatka1111@gmail.com>
2023-05-28 20:11:25 +0300

    Added a JS

--- src/script.js
+++ src/script.js
@@ -66,6 +66,40 @@ function showWeather(response) {
   displayTemperature(response);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon","Tue"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
+
+displayForecast();
+
+
 //current
 function currentlyPosition(position) {
   let apiKey = "7401c74fb46516853ce6e1c8d00fac73";
