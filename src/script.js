8da24dce65383a416f2b0163ed91e39879ee6e4a
DasNataliia <dasnatka1111@gmail.com>
2023-06-02 17:42:10 +0300

    Changed a JS

--- src/script.js
+++ src/script.js
@@ -32,6 +32,7 @@ celsiusTemperature= response.data.main.temp;
   windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute("src", iconUrl);
+  getForecast(response.data.coord);
 }
 
 
@@ -68,11 +69,9 @@ function showWeather(response) {
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon","Tue"];
-
-  let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
+let forecastHTML = `<div class="row">`;
+let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+days.forEach(function(day){
     forecastHTML =
       forecastHTML +
       `
@@ -89,18 +88,21 @@ function displayForecast() {
         </div>
       </div>
   `;
-  });
+});
+  
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "7401c74fb46516853ce6e1c8d00fac73";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
-displayForecast();
-
-
-//current
+//currents
 function currentlyPosition(position) {
   let apiKey = "7401c74fb46516853ce6e1c8d00fac73";
   let lat = position.coords.latitude;
@@ -141,3 +143,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let buttonLink = document.querySelector("#current");
 buttonLink.addEventListener("click", currentLocation);
+displayForecast();
