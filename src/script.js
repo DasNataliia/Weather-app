2ae9b3abeca235ad4cfe93664957cde6bed38f78
DasNataliia <dasnatka1111@gmail.com>
2023-06-04 11:10:58 +0300

    Changed a JS

--- src/script.js
+++ src/script.js
@@ -117,7 +117,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "7401c74fb46516853ce6e1c8d00fac73";
+  let apiKey = "5da7b2dc058f07286fea39c4cee516a3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -164,4 +164,3 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let buttonLink = document.querySelector("#current");
 buttonLink.addEventListener("click", currentLocation);
-displayForecast();
